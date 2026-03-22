6d2b8ace4c5dc1ce76ca2c059f843f54667e7aff
EDU11QR <quisperojasedu@gmail.com>
2026-03-21 21:10:45 -0500

    Primera clase

---
+++ WebApplication2026/Controllers/VehiculoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2026.Controllers
+{
+    public class VehiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
